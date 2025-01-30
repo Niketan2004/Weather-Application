25080bcf9ba1a474455be8b5ddb1469465bbdad6
Niketan2004 <Koyandeniketans2004@gmail.com>
2025-01-30 19:29:27 +0530

    made fully working weather application

--- src/main/java/com/project/Weather_Application/Controller/WeatherController.java
+++ src/main/java/com/project/Weather_Application/Controller/WeatherController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.Weather_Application.Model.WeatherModel;
@@ -28,7 +27,7 @@ public class WeatherController {
                model.addAttribute("weatherData", weatherData);
                return "weather"; // Your weather details page template
           } catch (Exception e) {
-               System.out.println( e.getMessage());
+               System.out.println(e.getMessage());
                model.addAttribute("errorMessage", "City not found.");
                return "index"; // Redirect back to the landing page with the error message
           }
--- src/main/java/com/project/Weather_Application/Model/WeatherModel.java
+++ src/main/java/com/project/Weather_Application/Model/WeatherModel.java
@@ -1,6 +1,5 @@
 package com.project.Weather_Application.Model;
 
-import java.sql.Date;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
